7acc0a5194eaf61162a568bc5e361ab317f57ab0
Tales Coelho dos Santos <tales-coelho@live.com>
2021-12-03 06:55:10 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
-app.use('/', user);
+app.use('/', user)
 app.use('/', post);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
